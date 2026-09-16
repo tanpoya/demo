d7ba0f38b272d4f0df64c2d7d89edd653f9a20fa
tanpoya <tanpoya02@gmail.com>
2026-09-16 14:18:25 +0900

    3주차

--- src/main/java/com/example/demo/Demo0ontroller.java
+++ src/main/java/com/example/demo/Demo0ontroller.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller // 컨트롤러 어노테이션 명시
 public class Demo0ontroller {
